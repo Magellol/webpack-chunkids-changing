b16f7cf46b327780706875af2abc89e2af14a8b1
Thomas Lefebvre <thomas@unsplash.com>
2020-07-09 09:46:20 -0400

    Setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const Path = require('path');
+
+const ROOT_PATH = __dirname;
+const TARGET_PATH = Path.join(ROOT_PATH, './target/');
+const SRC_PATH = Path.join(ROOT_PATH, './src/');
+
+const ENTRY_FILENAME = 'index.js';
+// const OUTPUT_FILENAME = '[name].[chunkhash:5].js';
+const OUTPUT_FILENAME = '[name].[contenthash:5].js';
+
+const config = {
+    mode: 'production',
+    entry: Path.resolve(SRC_PATH, ENTRY_FILENAME),
+    output: {
+        path: TARGET_PATH,
+        filename: OUTPUT_FILENAME,
+    },
+    optimization: {
+        minimize: false,
+        splitChunks: {
+            chunks: 'all',
+            minSize: 0,
+            maxAsyncRequests: 20,
+            maxInitialRequests: 20,
+        },
+    },
+};
+
+module.exports = config;
